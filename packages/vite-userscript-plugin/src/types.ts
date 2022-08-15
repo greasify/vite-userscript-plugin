b6b01978759a64db209744e3472e50e07721d2ee
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-15 10:04:19 +0800

    add typings

--- packages/vite-userscript-plugin/src/types.ts
+++ packages/vite-userscript-plugin/src/types.ts
@@ -1,7 +1,47 @@
+type RunAt =
+  | 'none'
+  | 'document-start'
+  | 'document-body'
+  | 'document-end'
+  | 'document-idle'
+  | 'context-menu'
+
+type Grants =
+  | 'GM_setValue'
+  | 'GM_getValue'
+  | 'GM_deleteValue'
+  | 'GM_setClipboard'
+  | 'GM_addStyle'
+  | 'GM_addElement'
+  | 'GM_listValues'
+  | 'GM_addValueChangeListener'
+  | 'GM_removeValueChangeListener'
+  | 'GM_log'
+  | 'GM_registerMenuCommand'
+  | 'GM_unregisterMenuCommand'
+  | 'GM_openInTab'
+  | 'GM_xmlhttpRequest'
+  | 'GM_download'
+  | 'GM_getTab'
+  | 'GM_saveTab'
+  | 'GM_getTabs'
+  | 'GM_notification'
+  | 'GM_info'
+
 export interface UserscriptPluginOptions {
+  [property: string]: string | boolean | string[]
   name: string
   namespace: string
   version: string
   author: string
   description: string
+  homepage: string
+  icon: string
+  include: string[] | string
+  exclude: string[] | string
+  match: string[] | string
+  require: string[] | string
+  connect: string
+  'run-at': RunAt
+  grant: Grants[]
 }
