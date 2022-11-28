9f85df0e28096c7444eb9d57456e7779d6facd5c
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-11-28 17:32:23 +0800

    chore: added catching error, refactor generateBundle

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,8 @@ import {
   grants,
   regexpScripts,
   regexpStyles,
-  styleTemplate
+  styleTemplate,
+  vitePluginName
 } from './constants.js'
 import css from './css.js'
 import { defineGrants, removeDuplicates, transform } from './helpers.js'
@@ -27,181 +28,189 @@ export type { UserscriptPluginConfig }
 export default function UserscriptPlugin(
   config: UserscriptPluginConfig
 ): PluginOption {
-  let pluginConfig: ResolvedConfig
-  let isBuildWatch: boolean
-  let socketConnection: connection | null = null
+  try {
+    let pluginConfig: ResolvedConfig
+    let isBuildWatch: boolean
+    let socketConnection: connection | null = null
 
-  const workdir = dirname(fileURLToPath(import.meta.url))
-  const logger = createLogger('info', {
-    prefix: '[vite-userscript-plugin]',
-    allowClearScreen: true
-  })
-
-  const httpServer = createServer((req, res) => {
-    return serveHandler(req, res, {
-      public: pluginConfig.build.outDir
+    const workdir = dirname(fileURLToPath(import.meta.url))
+    const logger = createLogger('info', {
+      prefix: `[${vitePluginName}]`,
+      allowClearScreen: true
     })
-  })
 
-  const WebSocketServer = server
-  const ws = new WebSocketServer({ httpServer })
-  ws.on('request', (request) => {
-    socketConnection = request.accept(null, request.origin)
-  })
-
-  return {
-    name: 'vite-userscript-plugin',
-    apply: 'build',
-    config() {
-      return userConfig(config)
-    },
-    async configResolved(userConfig) {
-      pluginConfig = userConfig
-      isBuildWatch = (userConfig.build.watch ?? false) as boolean
-      config.entry = resolve(userConfig.root, config.entry)
-      config.header.name = sanitize(config.header.name)
-
-      Array.from([
-        'match',
-        'require',
-        'include',
-        'exclude',
-        'resource',
-        'connect'
-      ]).forEach((key) => {
-        const value = config.header[key]
-        config.header[key] = removeDuplicates(value)
+    const httpServer = createServer((req, res) => {
+      return serveHandler(req, res, {
+        public: pluginConfig.build.outDir
       })
+    })
 
-      config.server = {
-        port: await getPort(),
-        open: false,
-        ...config.server
-      }
-    },
-    async transform(src: string, path: string) {
-      let code = src
+    const WebSocketServer = server
+    const ws = new WebSocketServer({ httpServer })
+    ws.on('request', (request) => {
+      socketConnection = request.accept(null, request.origin)
+    })
 
-      if (regexpStyles.test(path)) {
-        code = await css.add(src, path)
-      }
+    return {
+      name: vitePluginName,
+      apply: 'build',
+      config() {
+        return userConfig(config)
+      },
+      async configResolved(userConfig) {
+        pluginConfig = userConfig
+        isBuildWatch = (userConfig.build.watch ?? false) as boolean
+        config.entry = resolve(userConfig.root, config.entry)
+        config.header.name = sanitize(config.header.name)
 
-      if (path.includes(config.entry)) {
-        code = src + styleTemplate
-      }
+        Array.from([
+          'match',
+          'require',
+          'include',
+          'exclude',
+          'resource',
+          'connect'
+        ]).forEach((key) => {
+          const value = config.header[key]
+          config.header[key] = removeDuplicates(value)
+        })
 
-      return {
-        code,
-        map: null
-      }
-    },
-    generateBundle(_, bundle) {
-      for (const [_, file] of Object.entries(bundle)) {
-        const modules = Object.keys(
-          (file as unknown as { modules: string[] }).modules
-        )
-
-        const styleModules = modules.filter((module) =>
-          regexpStyles.test(module)
-        )
-
-        if (styleModules.length) {
-          css.merge(styleModules)
+        config.server = {
+          port: await getPort(),
+          open: false,
+          ...config.server
         }
-      }
-    },
-    async writeBundle(_, bundle) {
-      const { open, port } = config.server!
-      const userFilename = `${config.header.name}.user.js`
-      const proxyFilename = `${config.header.name}.proxy.user.js`
-      const metaFilename = `${config.header.name}.meta.js`
+      },
+      async transform(src: string, path: string) {
+        let code = src
 
-      for (const [fileName] of Object.entries(bundle)) {
-        if (regexpScripts.test(fileName)) {
-          const rootDir = pluginConfig.root
-          const outDir = pluginConfig.build.outDir
+        if (regexpStyles.test(path)) {
+          code = await css.add(src, path)
+        }
 
-          const outPath = resolve(rootDir, outDir, fileName)
-          const userFilePath = resolve(rootDir, outDir, userFilename)
-          const proxyFilePath = resolve(rootDir, outDir, proxyFilename)
-          const metaFilePath = resolve(rootDir, outDir, metaFilename)
-          const wsPath = resolve(workdir, `ws-${config.header.name}.js`)
+        if (path.includes(config.entry)) {
+          code = src + styleTemplate
+        }
 
-          try {
-            let source = readFileSync(outPath, 'utf8')
-            source = source.replace(styleTemplate, `${css.inject()}`)
-            source = await transform({
-              file: source,
-              name: fileName,
-              loader: 'js'
-            })
+        return {
+          code,
+          map: null
+        }
+      },
+      generateBundle(_, bundle) {
+        for (const outputChunk of Object.values(bundle)) {
+          if (outputChunk.type === 'asset') {
+            continue
+          }
 
-            config.header.grant = removeDuplicates(
-              isBuildWatch
-                ? grants
-                : [...defineGrants(source), ...(config.header.grant ?? [])]
-            )
+          // prettier-ignore
+          const styleModules = Object
+            .keys(outputChunk.modules)
+            .filter((module) => regexpStyles.test(module))
 
-            if (isBuildWatch) {
-              const wsFile = readFileSync(resolve(workdir, 'ws.js'), 'utf8')
+          if (styleModules.length) {
+            css.merge(styleModules)
+          }
+        }
+      },
+      async writeBundle(_, bundle) {
+        const { open, port } = config.server!
+        const userFilename = `${config.header.name}.user.js`
+        const proxyFilename = `${config.header.name}.proxy.user.js`
+        const metaFilename = `${config.header.name}.meta.js`
 
-              const wsScript = await transform({
-                file: wsFile.replace('__WS__', `ws://localhost:${port}`),
-                name: wsPath,
+        for (const [fileName] of Object.entries(bundle)) {
+          if (regexpScripts.test(fileName)) {
+            const rootDir = pluginConfig.root
+            const outDir = pluginConfig.build.outDir
+
+            const outPath = resolve(rootDir, outDir, fileName)
+            const userFilePath = resolve(rootDir, outDir, userFilename)
+            const proxyFilePath = resolve(rootDir, outDir, proxyFilename)
+            const metaFilePath = resolve(rootDir, outDir, metaFilename)
+            const wsPath = resolve(workdir, `ws-${config.header.name}.js`)
+
+            try {
+              let source = readFileSync(outPath, 'utf8')
+              source = source.replace(styleTemplate, `${css.inject()}`)
+              source = await transform({
+                file: source,
+                name: fileName,
                 loader: 'js'
               })
 
-              writeFileSync(wsPath, wsScript)
-              writeFileSync(
-                proxyFilePath,
-                new Banner({
-                  ...config.header,
-                  require: [
-                    ...config.header.require!,
-                    'file://' + wsPath,
-                    'file://' + outPath
-                  ]
-                }).generate()
+              config.header.grant = removeDuplicates(
+                isBuildWatch
+                  ? grants
+                  : [...defineGrants(source), ...(config.header.grant ?? [])]
               )
-            }
 
-            const banner = new Banner(config.header).generate()
-            writeFileSync(outPath, source)
-            writeFileSync(metaFilePath, banner)
-            writeFileSync(userFilePath, `${banner}\n\n${source}`)
-          } catch (err) {
-            console.log(err)
+              if (isBuildWatch) {
+                const wsFile = readFileSync(resolve(workdir, 'ws.js'), 'utf8')
+
+                const wsScript = await transform({
+                  file: wsFile.replace('__WS__', `ws://localhost:${port}`),
+                  name: wsPath,
+                  loader: 'js'
+                })
+
+                writeFileSync(wsPath, wsScript)
+                writeFileSync(
+                  proxyFilePath,
+                  new Banner({
+                    ...config.header,
+                    require: [
+                      ...config.header.require!,
+                      'file://' + wsPath,
+                      'file://' + outPath
+                    ]
+                  }).generate()
+                )
+              }
+
+              const banner = new Banner(config.header).generate()
+              writeFileSync(outPath, source)
+              writeFileSync(metaFilePath, banner)
+              writeFileSync(userFilePath, `${banner}\n\n${source}`)
+            } catch (err) {
+              console.log(err)
+            }
+          }
+        }
+
+        if (isBuildWatch && !httpServer.listening) {
+          const link = `http://localhost:${port}`
+          httpServer.listen(port, () => {
+            logger.clearScreen('info')
+            logger.info(colors.blue(`Running at: ${colors.gray(link)}`))
+          })
+
+          if (open) {
+            await openLink(`${link}/${proxyFilename}`)
+          }
+        } else if (!isBuildWatch) {
+          httpServer.close()
+          process.exit(0)
+        }
+      },
+      buildEnd() {
+        if (isBuildWatch) {
+          logger.clearScreen('info')
+
+          if (socketConnection) {
+            socketConnection.sendUTF(
+              JSON.stringify({
+                message: 'reload'
+              })
+            )
           }
         }
       }
-
-      if (isBuildWatch && !httpServer.listening) {
-        const link = `http://localhost:${port}`
-        httpServer.listen(port, () => {
-          logger.clearScreen('info')
-          logger.info(colors.blue(`Running at: ${colors.gray(link)}`))
-        })
-
-        if (open) {
-          await openLink(`${link}/${proxyFilename}`)
-        }
-      } else if (!isBuildWatch) {
-        httpServer.close()
-        process.exit(0)
-      }
-    },
-    buildEnd() {
-      if (isBuildWatch) {
-        logger.clearScreen('info')
-
-        if (socketConnection) {
-          socketConnection.sendUTF(
-            JSON.stringify({
-              message: 'reload'
-            })
-          )
-        }
-      }
+    }
+  } catch (err) {
+    console.error(err)
+    return {
+      name: vitePluginName
     }
   }
 }
