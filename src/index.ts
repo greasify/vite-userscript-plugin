2249c4ffa0b38c89f5488d2481041995cd41b617
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-03-26 21:51:40 +0800

    fix: disable minify on library mode

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,7 @@ export default function UserscriptPlugin(
       config() {
         return {
           build: {
+            minify: false,
             lib: {
               entry: config.entry,
               name: config.header.name,
